f65ddb5d42398fda833e8768da434d4b0a99a94b
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-26 21:05:59 -0600

    added main section to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,19 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className='text-white'>Home</div>
+      <div id="main" className="w-full h-screen text-center text-white">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <h1 className="py-4 uppercase">Home</h1>
+          <p className="py-4 md:max-w-[70%] m-auto">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+            quisquam explicabo nulla voluptate cupiditate doloremque eos est
+            aliquid eius. Excepturi ipsa ea ipsum iste beatae placeat quam earum
+            neque maxime!
+          </p>
+        </div>
+      </div>
+    </div>        
     </>
   )
 }
